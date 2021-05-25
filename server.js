2979135eb14b27119ce3769890a0fc4077b2206b
babaralishah <babar.bscs3261@iiu.edu.pk>
2021-05-25 15:35:41 +0500

    env revisited

--- server.js
+++ server.js
@@ -1,44 +1,44 @@
-const dotenv = require("dotenv");
+// const dotenv = require("dotenv");
 
-dotenv.config();
-const express = require('express');
-const bodyParser = require('body-parser');
+// dotenv.config();
+// const express = require('express');
+// const bodyParser = require('body-parser');
 
-// create express app
-const app = express();
+// // create express app
+// const app = express();
 
-// parse requests of content-type - application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }))
+// // parse requests of content-type - application/x-www-form-urlencoded
+// app.use(bodyParser.urlencoded({ extended: true }))
 
-// parse requests of content-type - application/json
-app.use(bodyParser.json())
+// // parse requests of content-type - application/json
+// app.use(bodyParser.json())
 
-// Configuring the database
-const dbConfig = require('./config/database.config.js');
-const mongoose = require('mongoose');
+// // Configuring the database
+// const dbConfig = require('./config/database.config.js');
+// const mongoose = require('mongoose');
 
-mongoose.Promise = global.Promise;
+// mongoose.Promise = global.Promise;
 
-// Connecting to the database
-mongoose.connect(dbConfig.url, {
-    useNewUrlParser: true
-}).then(() => {
-    console.log("Successfully connected to the database");
-}).catch(err => {
-    console.log('Could not connect to the database. Exiting now...', err);
-    process.exit();
-});
+// // Connecting to the database
+// mongoose.connect(dbConfig.url, {
+//     useNewUrlParser: true
+// }).then(() => {
+//     console.log("Successfully connected to the database");
+// }).catch(err => {
+//     console.log('Could not connect to the database. Exiting now...', err);
+//     process.exit();
+// });
 
-// define a simple route
-app.get('/', (req, res) => {
-    res.json({ "message": "Welcome to EasyNotes application. Take notes quickly. Organize and keep track of all your notes." });
-});
+// // define a simple route
+// app.get('/', (req, res) => {
+//     res.json({ "message": "Welcome to EasyNotes application. Take notes quickly. Organize and keep track of all your notes." });
+// });
 
-// Require Notes routes
-require('./app/routes/note.routes')(app);
+// // Require Notes routes
+// require('./app/routes/note.routes')(app);
 
 
-// listen for requests
-app.listen(3000, () => {
-    console.log("Server is listening on port 3000");
-});
+// // listen for requests
+// app.listen(3000, () => {
+//     console.log("Server is listening on port 3000");
+// });
